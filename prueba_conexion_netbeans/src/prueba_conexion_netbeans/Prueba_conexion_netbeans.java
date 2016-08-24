578d82605b17827e22a18409a548e5cafa2d9e77
sara <sara@localhost>
2016-08-23 20:40:50 -0500

    prueba 2 conexion netbeans github

--- prueba_conexion_netbeans/src/prueba_conexion_netbeans/Prueba_conexion_netbeans.java
+++ prueba_conexion_netbeans/src/prueba_conexion_netbeans/Prueba_conexion_netbeans.java
@@ -17,7 +17,8 @@ public class Prueba_conexion_netbeans {
     public static void main(String[] args) {
         
         
-        System.out.println("Hola mundo");
+        System.out.println("Hola");
+        System.out.println("prueba conexion netbeans github");
         
     }
     
